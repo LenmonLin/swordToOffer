4e84a450617f33e3733343c39d3783b9d27885b4
lemonLin <linfromearth@outlook.com>
2019-03-03 20:56:29 +0800

    "树(普通树)中两个结点的最低公共祖先"

--- src/GetLastCommonParent50.java
+++ src/GetLastCommonParent50.java
@@ -1,6 +1,9 @@
 import common.MakeTree;
 import common.TreeNodeCommon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author LemonLin
  * @Description :GetLastCommonParent
@@ -15,12 +18,20 @@ import common.TreeNodeCommon;
  *      二、考虑正确的输入结果
  *      三、考虑向左递归还是向右递归
  *
- * 解放二：假设是普通的树，但是每个子结点都有指向父结点的指针，这样的话类似与
+ * 解法二：假设是普通的树，但是每个子结点都有指向父结点的指针，这样的话类似与
  * 前面的链表找公共结点一样。
  * （ 总体思路：1、首先要知道链表是单向链表。如果两个单向链表有公共的结点，那么两个链表从某一个结点开始，
  *   它们的next都指向同一个结点。
  *   2、先求出两个链表的长度，进而求出两个链表的长度差，然后先遍历长的链表长度差，然后同时出发，当
  *   遍历了同一个结点的时候，就是公共结点）
+ *
+ *   解法三：假设是普通的树
+ *   思路：分两步，
+ *      一、分别记录根结点到两个目标结点的路径;
+ *              利用的是图的深度优先遍历的简化版来记录根结点到目标节点的路径
+ *
+ *      二、然后把题目转换为求两个链表的最后的公共结点;
+ *              默认是根结点到叶子节点的情况，两个链表一样长
  *
  */
 public class GetLastCommonParent50 {
@@ -53,15 +64,89 @@ public class GetLastCommonParent50 {
         return  treeNodeCommon;
     }
 
+
+    //获取路径的方法
+   public  void  getNodePath(TreeNodeCommon tRoot, TreeNodeCommon targetNode,
+                             ArrayList tempList ,ArrayList path){
+        if (tRoot == targetNode || tRoot == null){
+            return ;
+        }
+        tempList.add(tRoot);
+
+       ArrayList<TreeNodeCommon> childs = new ArrayList<>();
+        if (tRoot.left != null){
+           childs.add(tRoot.left);
+        }
+
+       if (tRoot.right!= null){
+           childs.add(tRoot.right);
+       }
+
+        for (TreeNodeCommon node :childs ){
+
+            //注意这里用的是比较data,不能直接比较结点，否则会包括比较结点下面的子结点，导致无法进入if
+            if (node.data == targetNode.data){
+                path.addAll(tempList);
+                break;
+            }
+            getNodePath(node,targetNode,tempList,path);
+        }
+
+        //不包括目标节点，把目标节点排除
+        tempList.remove(tempList.size()-1);
+   }
+
+   //获取两个路径的最后一个公共结点，重点是最后两个字
+   public  TreeNodeCommon getLastCommonParent(ArrayList<TreeNodeCommon> path1,
+                                        ArrayList<TreeNodeCommon> path2){
+
+
+       TreeNodeCommon treeNode  = null;
+        for (int i=0;i<path1.size();i++){
+            if (path1.get(i)!=path2.get(i)){
+                break;
+            }
+
+            treeNode = path1.get(i);
+        }
+        return treeNode;
+   }
+
+
+   public  TreeNodeCommon GetLastCommonParentTwo(TreeNodeCommon tRoot ,
+                                                 TreeNodeCommon node1 ,TreeNodeCommon node2){
+        ArrayList path1 = new ArrayList();
+        ArrayList path2 = new ArrayList();
+
+        ArrayList temp = new ArrayList();
+
+
+        getNodePath(tRoot,node1,temp,path1);
+        getNodePath(tRoot,node2,temp,path2);
+
+        //如果路径不存在，返回空
+       if (path1.size()==0 || path2.size() ==0){
+           return null;
+       }
+
+       return getLastCommonParent(path1,path2);
+   }
+
     public static void main(String[] args) {
         MakeTree makeTree = new MakeTree();
 
         TreeNodeCommon pRoot = makeTree.makeTree();
         TreeNodeCommon tLeft = new TreeNodeCommon(5);
-        TreeNodeCommon tRight = new TreeNodeCommon(11);
+        TreeNodeCommon tRight = new TreeNodeCommon(7);
 
         GetLastCommonParent50 getLastCommonParent50 = new GetLastCommonParent50();
-        TreeNodeCommon lastCommonParent = getLastCommonParent50.GetLastCommonParentOne(pRoot,tLeft,tRight);
+        //TreeNodeCommon lastCommonParent = getLastCommonParent50.GetLastCommonParentOne(pRoot,tLeft,tRight);
+        //System.out.println(lastCommonParent.data);
+
+
+        TreeNodeCommon lastCommonParent = getLastCommonParent50.GetLastCommonParentTwo(pRoot
+        ,tLeft,tRight);
         System.out.println(lastCommonParent.data);
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,22 @@
-///**
-// * @author LemonLin
-// * @Description :Main
-// * @date 2018/4/3-15:20
-// */
-//import java.io.*;
-//import java.util.*;
-//public class Main
-//{
-//    public static void main(String args[])
-//    {
-//        Scanner cin = new Scanner(System.in);
-//        int a, b;
-//        int temp=0;
-//        int [] arr;
-//
-//        while (cin.hasNextInt()){
-//            a=cin.nextInt();
-//            b=cin.nextInt();
-//
-//
-//            for (int k = 0;k<b*3;++k){
-//                arr[k]=cin.nextInt();
-//            }
-//            System.out.println("YES");
-//            if(arr[0]==0&&arr[1]==1
-//            )
-//
-//            if(a<b){
-//                temp = b;
-//                b=a;
-//                a=temp;
-//            }
-//            while (a%b!=0){
-//                temp = a%b;
-//                a= b;
-//                b=temp;
-//            }
-//            System.out.println(b);
-//            int [] array = new int[];
-//            int n=0;
-//            int i=0;
-//            for (i=0;i<n;++i){
-//
-//            }
-//
-//            if(int[i]==temp){
-//                System.out.println("YES");
-//            }
-//            //        ArrayList arrayList = new ArrayList();
-///*            arrayList.add(cin.nextInt());
-//            System.out.println(arrayList);*/
-//
-//        }
-//
-//// 输入数组;
-///*        Scanner sc = new Scanner(System.in);
-//        int n= sc.nextInt();
-//        int[] arr = new int[n];
-//        for (int i = 0; i < n; ++i) {
-//            arr[i] = sc.nextInt();
-//        }*/
-//
-////二维数组
-//        /*Scanner scan = new Scanner(System.in);
-//        int t = scan.nextInt();
-//        int[][] input = new int[t][t];
-//        for(int i=0;i<input.length;i++){
-//            for(int j=0;j<input[i].length;j++){
-//                input[i][j]=scan.nextInt();
-//            }
-//
-//        }*/
-////        System.out.println(input);
-////        for(int i=0;i<input.length;i++){
-////            for(int j=0;j<input[i].length;j++){
-////                System.out.println(input[i][j]);
-////            }
-////
-////        }
-////        while(cin.hasNextInt())
-////        {
-////            a = cin.nextInt();
-////            b = cin.nextInt();
-////            System.out.println(a + b);
-////        }
-//
-////        String str1,str2;
-////        while (cin.hasNextLine()){
-////            str1=cin.nextLine();
-////            str2 = cin.nextLine();
-////            System.out.println(str1+str2);
-////        }
-//    }
-//
-//    public  int check(int a,int b){
-//
-//        for (int i=0;i<length;i+=3){
-//            if (arr[i]==-1)
-//            arr[i]=i+1;
-//            else
-//
-//        }
-//
-//    }
-//
-//
-//}
+/**
+ * @author LemonLin
+ * @Description :Main
+ * @date 2018/4/3-15:20
+ */
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    public static void main(String args[]){
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+
+        arrayList.remove(arrayList.size()-1);
+        for (Object a:arrayList){
+            System.out.println(a);
+        }
+    }
+
+}
