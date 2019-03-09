33c9f55b7e12622850c6a51629599d0dcca91569
lemonLin <linfromearth@outlook.com>
2019-03-09 09:38:12 +0800

    二叉树的下一个结点

--- src/TreeGetNext58.java
+++ src/TreeGetNext58.java
@@ -22,8 +22,8 @@ import common.TreeNodeCommon;
  * 1、有右子树的，那么下个结点就是右子树最左边的点；（eg：D，B，E，A，C，G）
  * 2、没有右子树的，也可以分成两类，
  * a)是父节点左孩子（eg：N，I，L） ，那么父节点就是下一个节点 ；
- * b)是父节点的右孩子（eg：H，J，K，M）找他的父节点的父节点的父节点...直到当前结点是其父节点的左孩子位置。
- * c) 如果没有eg：M，那么他就是尾节点。
+ * b1)是父节点的右孩子（eg：H，J，K，M）找他的父节点的父节点的父节点...直到当前结点是其父节点的左孩子位置。
+ * b2) 如果没有eg：M，那么他就是尾节点。
  */
 public class TreeGetNext58 {
     public TreeLinkNode GetNext(TreeLinkNode pNode) {
