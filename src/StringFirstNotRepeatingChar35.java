d2a3a4823fa405f9872800d0d10862968e5dfd5f
lemonLin <linfromearth@outlook.com>
2019-03-09 16:48:37 +0800

    第一个只出现一次的字符

--- src/StringFirstNotRepeatingChar35.java
+++ src/StringFirstNotRepeatingChar35.java
@@ -1,11 +1,11 @@
-package common;
-
-import java.util.HashMap;
-
 /**
  * @author LemonLin
  * @Description :FirstNotRepeatingChar35
  * @date 2018/4/16-15:27
+ *
+ * 题目描述
+ * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的
+ * 位置, 如果没有则返回 -1（需要区分大小写）.
  *
  * 第一点，考虑用hash表的时候，字符不要惯性的认为只有26个英文字符，还有大写，-、+等符号字符，
  * 所以根据字符的位数，应该是256位字符。
@@ -13,7 +13,7 @@ import java.util.HashMap;
  * 第三点，字符是ASCII编码，所以转换为数字下标，其实可以直接用即可。
  * 第四点，其实不用使用hash表，应为key值是数字，所以使用数组即可;
  */
-public class FirstNotRepeatingChar35 {
+public class StringFirstNotRepeatingChar35 {
     public int FirstNotRepeatingChar(String str) {
 
         int [] intsMap = new int [256];
@@ -37,9 +37,9 @@ public class FirstNotRepeatingChar35 {
     public static void main(String[] args) {
 
         String string = new String("abaccdeff");
-        FirstNotRepeatingChar35 firstNotRepeatingChar35 = new FirstNotRepeatingChar35();
+        StringFirstNotRepeatingChar35 stringFirstNotRepeatingChar35 = new StringFirstNotRepeatingChar35();
 
-        int result = firstNotRepeatingChar35.FirstNotRepeatingChar(string);
+        int result = stringFirstNotRepeatingChar35.FirstNotRepeatingChar(string);
         System.out.println(result);
 
     }
