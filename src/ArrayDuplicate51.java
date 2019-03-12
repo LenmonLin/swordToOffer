e2f782d821727aba912ccfb1dd7b2f331595311b
lemonLin <linfromearth@outlook.com>
2019-03-12 16:58:30 +0800

    数组中重复的数字

--- src/ArrayDuplicate51.java
+++ src/ArrayDuplicate51.java
@@ -3,7 +3,7 @@
  * @Description :Duplicate51
  * @date 2018/9/20-11:29
  *
- * 数组中重复的数字:
+ * 题目描述：数组中重复的数字:
  * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。
  * 也不知道每个数字重复几次。请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，
  * 那么对应的输出是第一个重复的数字2。
@@ -23,13 +23,8 @@
  *       如果不是，再拿它和第m个数字进行比较。
  *              如果它和第m个数字相等，就找到了一个重复的数字
  *              如果不相等，就把第i个数字和第m个数字交换，把m放到属于它的位置
- *
- *
- *
- *
- *
  */
-public class Duplicate51 {
+public class ArrayDuplicate51 {
 
     /**
      *@Description
@@ -66,10 +61,10 @@ public class Duplicate51 {
     public static void main(String[] args) {
 
         int test[]={2,3,1,0,2,5,3};
-        Duplicate51 duplicate51 =new Duplicate51();
+        ArrayDuplicate51 arrayDuplicate51 =new ArrayDuplicate51();
 
         int duplicate[]={0};
-        duplicate51.duplicate(test,test.length,duplicate);
+        arrayDuplicate51.duplicate(test,test.length,duplicate);
 
         System.out.println(duplicate[0]);
 
