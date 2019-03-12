ba9cf286ae5e5b210b5d06177a0b0c3c4d133d44
lemonLin <linfromearth@outlook.com>
2019-03-12 16:46:00 +0800

    数组中出现次数超过一半的数字

--- src/ArrayMoreThanHalfNumber29.java
+++ src/ArrayMoreThanHalfNumber29.java
@@ -5,10 +5,16 @@ import java.util.Map;
  * @author LemonLin
  * @Description :MoreThanHalfNumber29
  * @date 2018/4/11-14:51
+ *
+ * 题目描述
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,
+ * 2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ *
+ * 解题思路：
  * 考虑用hashMap记录数组中出现的次数;
  * 空间换时间
  */
-public class MoreThanHalfNumber29 {
+public class ArrayMoreThanHalfNumber29 {
     public int MoreThanHalfNum_Solution(int [] array){
         Map<Integer,Integer> map = new HashMap<>();
         //初始化map中的value值为0
@@ -29,8 +35,8 @@ public class MoreThanHalfNumber29 {
 
     public static void main(String[] args) {
         int [] array={1,2,3,2,2,2,5,4,2};
-        MoreThanHalfNumber29 moreThanHalfNumber29 = new MoreThanHalfNumber29();
-        int result = moreThanHalfNumber29.MoreThanHalfNum_Solution(array);
+        ArrayMoreThanHalfNumber29 arrayMoreThanHalfNumber29 = new ArrayMoreThanHalfNumber29();
+        int result = arrayMoreThanHalfNumber29.MoreThanHalfNum_Solution(array);
         System.out.println(result);
     }
 }
