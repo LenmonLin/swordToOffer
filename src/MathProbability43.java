41fc03f81351d9eb01c2c5a6c95b30c9419189c8
lemonLin <linfromearth@outlook.com>
2019-03-15 21:00:25 +0800

    n个骰子点数和及各自出现的概率

--- src/MathProbability43.java
+++ src/MathProbability43.java
@@ -10,17 +10,18 @@
  * 1、确定问题的解的表达式：
  * 2、将最终问题的构造成上一阶段问题的解，即递推公式
  *
- * 第一步：确定问题解的表达式：  种类数 f(diceNumber,sum) ：diceNumber表示为输入的骰子数，sum表示点数之和
+ * 第一步：确定问题解的表达式：  种类数 f(diceNumber,sum) ：diceNumber表示为输入的骰子数，
+ * sum表示点数之和
  *
  * 第二步：确定状态转移方程，包括初始状态。
- * n个骰子点数和为s的种类数只与n-1个骰子的和有关，因为一个骰子有六个点数，当第n个骰子点数为1的话，那么
- * f(diceNumber,sum)=f(diceNumber-1,sum-1);当第n-1个骰子点数为2的话，那么f(diceNumber,sum)=f(diceNumber-1,sum-2)
- * 以此内推；所以f(diceNumber,sum)=f(diceNumber-1,sum-1)+f(diceNumber-1,sum-2)+f(diceNumber-1,sum-3)+
+ * n个骰子点数和为s的种类数只与n-1个骰子的和有关，因为一个骰子有六个点数，当第n个骰子点数为1的话，
+ * 那么f(diceNumber,sum)=f(diceNumber-1,sum-1);当第n-1个骰子点数为2的话，那
+ * 么f(diceNumber,sum)=f(diceNumber-1,sum-2)以此内推；所以f(diceNumber,sum)=
+ * f(diceNumber-1,sum-1)+f(diceNumber-1,sum-2)+f(diceNumber-1,sum-3)+
  * f(diceNumber-1,sum-4)+f(diceNumber-1,sum-5)+f(diceNumber-1,sum-6);
- *
  *初始状态下f(1,1)=f(1,2)=f(1,3)=f(1,4)=f(1,5)=f(1,6)=1
  */
-public class Probability43 {
+public class MathProbability43 {
 
     /**
      *@Description
@@ -52,10 +53,10 @@ public class Probability43 {
          * 骰子的个数5
          */
         int Count=2;
-        Probability43 probability43 = new Probability43();
+        MathProbability43 mathProbability43 = new MathProbability43();
         for (int n=6*Count;n>=2;n--) {
             System.out.println("骰子数为"+Count+"===="+"和为"+n+"的个数为"+
-                    probability43.getSumCount(Count, n));
+                    mathProbability43.getSumCount(Count, n));
         }
 
     }
