0149aa6c62e4852deabde8888e9a9f05ff13ae14
lemonLin <linfromearth@outlook.com>
2019-03-15 20:04:13 +0800

    由前序遍历和中序遍历重建二叉树

--- src/TreeConstructBinaryTree06.java
+++ src/TreeConstructBinaryTree06.java
@@ -1,40 +1,31 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
 import common.TreeNodeCommon;
-
 /**
  * @author LemonLin
  * @Description :ConstructBinaryTree06
  * @date 2018/2/7-17:23
+ * 题目描述
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中
+ * 都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重
+ * 建二叉树并返回。
  *
+ * 解题思路：其实就是找根结点，找左子树，右子树下标带入递归函数
  *
  * 疑问点：
- *
  * Java中怎么表示指针，比如数组要用首指针和末尾指针表示；
  * 为解决这个问题，应该把数组首地址和数组的第一个下标和最后一个下标传入即可；
- *
  * Java中表示c语言中的结构体可以用类来表示，Java中没有结构体的说法。
- *
  * 重要解题要点:
  *      使用递归方法解题，一定要写出口代码，不然程序肯定会报错；
- *
- *
- *
  */
-
 /*public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
 }*/
-
-
-public class ConstructBinaryTree06 {
-
-
+public class TreeConstructBinaryTree06 {
     //中序遍历二叉树
-    public void InorderTraversal( TreeNodeCommon bt )
-    {
+    public void InorderTraversal( TreeNodeCommon bt ) {
         if(bt == null){
             return ;
         }
@@ -42,54 +33,37 @@ public class ConstructBinaryTree06 {
         System.out.println("========"+bt.data);
         InorderTraversal(bt.right);
     }
-
-
-
     public TreeNodeCommon reConstructBinaryTree(int [] preorder,int [] inorder) {
-
-
         return ConstructBinaryTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
-
     }
-
     public TreeNodeCommon ConstructBinaryTree(int [] preorder,int preStart,int preEnd,int [] inorder,int inStart,int inEnd) {
-
         TreeNodeCommon rootNode = new TreeNodeCommon(preorder[preStart]);
-
         int  rootValue = preorder[preStart];
         rootNode.left = rootNode.right = null;
-
         if(preorder.length == 0||inorder.length == 0){
             return null;
         }
-
         if (preStart == preEnd){
             if (inStart==inEnd&&preorder[preStart]==inorder[inStart]){
                 return rootNode;
             }
         }
         int  rootInorder=preStart;
-
         System.out.println("前序遍历的左半部分开头preStart==="+preStart);
 //        for (;rootInorder<=inEnd;rootInorder++){
 //            if (rootValue == inorder[rootInorder]){
 //                break;
 //            }
 //        }
-
         while (rootInorder <=inEnd && inorder[rootInorder]!=rootValue){
             rootInorder++;
         }
         System.out.println("中序遍历根节点的位置rootInorder="+rootInorder);
-
        // int leftInLength =in.length- i;//这里不能用这个，因为这样做算的是右子树的长度
         int leftInLength =rootInorder-inStart;//这里i是指的是中序系列的根节点的坐标，所以也要用instart,而不是prestart
         int leftPreorderEnd = preStart+leftInLength;
-
-
         System.out.println("in.length中序遍历的序列长度=="+inorder.length);
         System.out.println("leftInLength中序遍历的根节点的左半部分长度=="+leftInLength);
-
         //第一个参数固定pre
         //注意得判断左子树和右子树是否还有长度才再调用，否则递归出错
         if (leftInLength>0){
@@ -103,26 +77,16 @@ public class ConstructBinaryTree06 {
         }
         return rootNode;
     }
-
-
     public static void main(String[] args) {
-
         int []preorder={1,2,3,4};
-
         int [] inorder={4,3,2,1};
-
         //这里不能写TreeNodeCommon，不是返回什么就写什么；
         //新建一个什么类，自然就返回什么类的，类和方法不一样。
-
-        ConstructBinaryTree06 test = new ConstructBinaryTree06();
+        TreeConstructBinaryTree06 test = new TreeConstructBinaryTree06();
         TreeNodeCommon treeNodeCommon = test.reConstructBinaryTree(preorder, inorder);
-
         //打印二叉树
         test.InorderTraversal(treeNodeCommon);
-
-
     }
-
 }
 
 
