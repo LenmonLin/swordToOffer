2ad07521e382f036d1fb07e2d6226a1ae22c970c
lemonLin <linfromearth@outlook.com>
2019-03-15 20:19:07 +0800

    斐波那契数列

--- src/MathFibonacci09.java
+++ src/MathFibonacci09.java
@@ -1,14 +1,16 @@
 /**
- * 没有什么难度，就是不要用递归去处理，效率比较低
- * 正确的做法是保留中间的结果
  * @author LemonLin
  * @Description :Fibonacci09
  * @date 2018/2/25-11:48
+ * 题目描述
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * n<=39
+ *解题思路：
+ *  没有什么难度，就是不要用递归去处理，效率比较低
+ *  正确的做法是保留中间的结果
  */
-public class Fibonacci09 {
-
+public class MathFibonacci09 {
     public int Fibonacci(int n) {
-
         int result[]={0,1};
         if (n<=2){
             return result[n-1];
@@ -24,12 +26,11 @@ public class Fibonacci09 {
         }
         return fibonacciResult;
     }
-
     //测试代码
     public static void main(String[] args) {
-        Fibonacci09 fibonacci09 = new Fibonacci09();
+        MathFibonacci09 mathFibonacci09 = new MathFibonacci09();
         int n=1;
-        int fibonacciResult = fibonacci09.Fibonacci(n);
+        int fibonacciResult = mathFibonacci09.Fibonacci(n);
         System.out.println(fibonacciResult);
     }
 }
