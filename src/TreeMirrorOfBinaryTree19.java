c7e8d4b0fbd554128af9b91f81330245fd8106aa
lemonLin <linfromearth@outlook.com>
2019-03-16 20:17:25 +0800

    二叉树的镜像

--- src/TreeMirrorOfBinaryTree19.java
+++ src/TreeMirrorOfBinaryTree19.java
@@ -1,14 +1,18 @@
 import common.TreeNodeCommon;
-
 /**
+ * 题目描述
+ * 操作给定的二叉树，将其变换为源二叉树的镜像。
+ * 解题思路：
+ * 先前序遍历这棵树的每个结点，如果遍历到的结点有子结点，就交换它的两个子节点，
+ * 当交换完所有的非叶子结点的左右子结点之后，就得到了树的镜像。（主要就是设第三变量
+ * 交换左右结点的问题）
+ *
  * 考虑求镜像的退出条件，结点的左右子树都是空的情况
- * 之后就是交换指针的问题
  * @author LemonLin
  * @Description :MirrorOfBinaryTree19
  * @date 2018/2/25-20:38
  */
-public class MirrorOfBinaryTree19 {
-
+public class TreeMirrorOfBinaryTree19 {
     public void Mirror(TreeNodeCommon root) {
         if (root ==null)
             return;
@@ -25,6 +29,5 @@ public class MirrorOfBinaryTree19 {
         if (root.right != null){
             Mirror(root.right);
         }
-
     }
 }
