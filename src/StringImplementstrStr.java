95d9c21fe43f88ab1cda55f418a9caeeb16b6df6
lemonLin <linfromearth@outlook.com>
2019-05-22 20:55:45 +0800

    在长字符串中找短字符串简化版

--- src/StringImplementstrStr.java
+++ src/StringImplementstrStr.java
@@ -46,6 +46,24 @@ public class StringImplementstrStr {
         return -1;
     }
 
+    //简化版
+    public int strStr2(final String haystack, final String needle) {
+        if (needle.isEmpty()) return 0;
+
+        final int N = haystack.length() - needle.length() + 1;
+        for (int i = 0; i < N; i++) {
+            int j = i;
+            int k = 0;
+            while (j < haystack.length() && k < needle.length() &&
+                    haystack.charAt(j) == needle.charAt(k)) {
+                j++;
+                k++;
+            }
+            if (k == needle.length()) return i;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         String haystack = "mississippi", needle = "issipi";
         StringImplementstrStr stringImplementstrStr = new StringImplementstrStr();
