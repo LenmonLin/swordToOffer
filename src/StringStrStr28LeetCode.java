5cb082add1c7f51a66b25b3daac4a8f1ff838588
lemonLin <linfromearth@outlook.com>
2019-07-25 11:00:29 +0800

    给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。

--- src/StringStrStr28LeetCode.java
+++ src/StringStrStr28LeetCode.java
@@ -1,13 +1,7 @@
 /**
- * @author LemonLin
- * @Description :StringImplementstrStr
- * @date 2019/5/20-20:41
- *Implement strStr().
- *    Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
- *直译：
  * 实现 strStr() 函数。
- * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位
- * 置 (从0开始)。如果不存在，则返回  -1。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle
+ * 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
  * Example 1:
  * Input: haystack = "hello", needle = "ll"
  * Output: 2
@@ -16,13 +10,19 @@
  * Output: -1
  *说明:
  * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
- * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
- *
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的
+ * indexOf() 定义相符。
  * 解题思路：
- *  使用两层循环暴力算法遍历，时间复制度为O(m*n)。固定needle[0]与每个haystack进行比较，如果相等，needle
- *  和haystack都下标加一，继续比较，直到比较到needle长度结束，返回下标。
+ *  使用两层循环暴力算法遍历，时间复制度为O(m*n)。固定needle[0]与每个haystack进行比较，如果
+ *  等，needle和haystack都下标加一，继续比较，直到比较到needle长度结束，返回下标。
+ * @author LemonLin
+ * @Description :StringImplementstrStr
+ * @date 2019/5/20-20:41
+ *Implement strStr().
+ *    Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part
+ *    of haystack.
  */
-public class StringImplementstrStr {
+public class StringStrStr28LeetCode {
     public int strStr(String haystack, String needle) {
         if (needle.isEmpty())return 0;
         int i=0;
@@ -66,7 +66,7 @@ public class StringImplementstrStr {
 
     public static void main(String[] args) {
         String haystack = "mississippi", needle = "issipi";
-        StringImplementstrStr stringImplementstrStr = new StringImplementstrStr();
+        StringStrStr28LeetCode stringImplementstrStr = new StringStrStr28LeetCode();
         int result = stringImplementstrStr.strStr(haystack, needle);
         System.out.println(result);
     }
