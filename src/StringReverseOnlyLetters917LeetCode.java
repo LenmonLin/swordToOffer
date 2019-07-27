1f6b9c2a9cab810a8b61e57b0be5cf6e83ef6baa
lemonLin <linfromearth@outlook.com>
2019-07-27 11:10:52 +0800

    给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转

--- src/StringReverseOnlyLetters917LeetCode.java
+++ src/StringReverseOnlyLetters917LeetCode.java
@@ -1,4 +1,8 @@
 /**
+ * 给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置
+ * 发生反转。
+ * 思路：
+ * 翻转思路是设置两个首尾指针，用一个中间的变量来存，首尾互相交换,翻转的终止条件是，首尾指针想遇
  * @author LemonLin
  * @Description :StringReverseOnlyLetters
  * @date 19.6.17-23:04
@@ -17,13 +21,8 @@
  * S.length <= 100
  * 33 <= S[i].ASCIIcode <= 122 
  * S doesn't contain \ or "
- * 给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置
- * 发生反转。
- *
- * 思路：
- * 翻转思路是设置两个首尾指针，用一个中间的变量来存，首尾互相交换,翻转的终止条件是，首尾指针想遇
  */
-public class StringReverseOnlyLetters {
+public class StringReverseOnlyLetters917LeetCode {
     public String reverseOnlyLetters(String S) {
         char[] chars = S.toCharArray();
         Reverse(chars,0,S.length()-1);
@@ -54,6 +53,6 @@ public class StringReverseOnlyLetters {
 
     public static void main(String[] args) {
         String string="7_28]";
-        System.out.println(new StringReverseOnlyLetters().reverseOnlyLetters(string));
+        System.out.println(new StringReverseOnlyLetters917LeetCode().reverseOnlyLetters(string));
     }
 }
