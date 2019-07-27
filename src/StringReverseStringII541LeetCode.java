91bc07d18a9413b96c978db3cd6721dd75597811
lemonLin <linfromearth@outlook.com>
2019-07-27 10:44:42 +0800

    给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。 如果剩余少于 k 个字符，则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符， 则反转前 k 个字符，并将剩余的字符保持原样。

--- src/StringReverseStringII541LeetCode.java
+++ src/StringReverseStringII541LeetCode.java
@@ -1,4 +1,10 @@
 /**
+*给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。
+ * 如果剩余少于 k 个字符，则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符，
+ * 则反转前 k 个字符，并将剩余的字符保持原样。
+ *思路：主要是题目的理解要清楚，比如s="abcdefg",给定k=2;要反转的是ab,不反转的是2k=2*2=4,
+ * 也就是第三，第四个不翻转，cd不翻转。然后接下去继续重复这个循环。
+ * 翻转思路是设置两个首尾指针，用一个中间的变量来存，首尾互相交换,翻转的终止条件是，首尾指针想遇
  * @author LemonLin
  * @Description :StringReverseStringII
  * @date 19.6.17-22:47
@@ -12,15 +18,8 @@
  * Restrictions:
  * The string consists of lower English letters only.
  * Length of the given string and k will in the range [1, 10000]
- *给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。
- * 如果剩余少于 k 个字符，则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符，
- * 则反转前 k 个字符，并将剩余的字符保持原样。
- *
- *思路：主要是题目的理解要清楚，比如s="abcdefg",给定k=2;要反转的是ab,不反转的是2k=2*2=4,
- * 也就是第三，第四个不翻转，cd不翻转。然后接下去继续重复这个循环。
- * 翻转思路是设置两个首尾指针，用一个中间的变量来存，首尾互相交换,翻转的终止条件是，首尾指针想遇
  */
-public class StringReverseStringII {
+public class StringReverseStringII541LeetCode {
     public String reverseStr(String s, int k) {
         char[] chars = s.toCharArray();
         boolean flag = false;
@@ -56,6 +55,6 @@ public class StringReverseStringII {
     }
     public static void main(String[] args) {
         String string = "abcdefghijklmn";
-        System.out.println(new StringReverseStringII().reverseStr(string, 3));
+        System.out.println(new StringReverseStringII541LeetCode().reverseStr(string, 3));
     }
 }
