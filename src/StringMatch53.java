5b2b3251faae85a123dba6b089ae409390c7cdd8
lemonLin <linfromearth@outlook.com>
2019-08-04 10:19:32 +0800

    请实现一个函数用来匹配包括'.'和'*'的正则表达式。

--- src/StringMatch53.java
+++ src/StringMatch53.java
@@ -1,24 +1,16 @@
 /**
- * @author LemonLin
- * @Description :Match53
- * @date 2019/3/5-15:06
- *
  *题目描述
  * 请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，
  * 而'*'表示它前面的字符可以出现任意次（包含0次）。在本题中，匹配是指字符串的所有
  * 字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"
  * 和"ab*a"均不匹配
- *
  * 解题bug：
  *      char[] str={}与char[] str=null 的区别
  *      char[] str={}与char[] str={' '}的区别
  *      用例:"","."牛客网无法通过
- *
  * 解题思路：
  *      总体情况是，str是需要匹配的字符串，一个pattern是需要匹配的模式，分别用
- *      strIndex,patternIndex,来比较字符。
- *      需要用到递归的思路来对数组下标进行匹配。
- *
+ *      strIndex,patternIndex,来比较字符。需要用到递归的思路来对数组下标进行匹配。
  *       总括：patternIndex指针先动，strIndex 指针再动
  *       if(pattern下一个字符是'*'){
  *           if(pattern当前字符是'.'){
@@ -35,8 +27,6 @@
  *       if(pattern下一个字符不是'*'){
  *           是否有含'.'
  *       }
- *
- *
  *      匹配规则上：
  *     首先，考虑特殊情况：
  *          1>str 和pattern都为空，返回true
@@ -59,41 +49,33 @@
  *                 不变。（这里匹配1个或多个可以看成一种情况，因为：当匹配一个时，
  *                 由于str移到了下一个字符，而pattern字符不变，就回到了上边的情况a；
  *                 当匹配多于一个字符时，相当于从str的下一个字符继续开始匹配）
- *
- *
- *
- *
+ * @author LemonLin
+ * @Description :Match53
+ * @date 2019/3/5-15:06
  */
 public class StringMatch53 {
 
     public boolean match(char[] str, char[] pattern) {
-
         if (str==null  ||pattern ==null){
             return false;
         }
-
         int strIndex = 0;
         int patternIndex =0;
         return matchCore(str,strIndex,pattern,patternIndex);
     }
-
     public boolean matchCore(char[] str,int strIndex,char[] pattern,int patternIndex){
-
         //匹配成功的出口是，str匹配到了尾部，pattern匹配到了尾部
         if (strIndex == str.length && patternIndex == pattern.length){
             return  true;
         }
-
         //匹配失败的出口是，pattern先到了尾部，而str还没到
         if (strIndex!=str.length && patternIndex == pattern.length){
             return  false;
         }
-
         //当越界的时候返回false
         if (strIndex >str.length ||patternIndex > pattern.length){
             return false;
         }
-
         //这里要限定数组下标不能越界
         if (patternIndex+1<pattern.length&&pattern[patternIndex+1]=='*'){
             //但pattern是这种模式的时候'abc.*bcd'
@@ -114,7 +96,6 @@ public class StringMatch53 {
                 return matchCore(str,strIndex,pattern,patternIndex+2);
             }
         }
-
         if (str[strIndex]==pattern[patternIndex]||(pattern[patternIndex]=='.'&&strIndex != str.length)){
             return matchCore(str,strIndex+1,pattern,patternIndex+1);
         }
@@ -122,7 +103,6 @@ public class StringMatch53 {
         return false;
 
     }
-
 
     public static void main(String[] args) {
         StringMatch53 stringMatch53 = new StringMatch53();
