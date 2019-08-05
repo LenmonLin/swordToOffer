6c9d082b1f9b086894104bce2e4662e0d5b57c3b
lemonLin <linfromearth@outlook.com>
2019-08-05 20:54:00 +0800

    请实现一个函数，将一个字符串中的每个空格替换成“%20”。

--- src/StringReplaceBlank04.java
+++ src/StringReplaceBlank04.java
@@ -1,19 +1,18 @@
 /**
  * 题目描述
- * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之
- * 后的字符串为We%20Are%20Happy。
- *
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are
+ * Happy.则经过替换之后的字符串为We%20Are%20Happy。
  * 总体思路：
- *      从后向前复制字符串；
+ *      从后向前复制字符串；（为何要从后往前，没有必要，也可以从前往后）
  *      先遍历一遍字符串，记录空格的数量和字符串的长度，确定最后生成的字符串的长度
+ *      开辟一个新的字符串数组来存放字符串和空格，遍历第二遍，遇到空格就填%20，否则
+ *      就是常规复制。
  *      java中遍历字符串使用的是charAt()方法定位每个字符
  * @author LemonLin
  * @Description :ReplaceBlank04
  * @date 2018/3/3-21:14
  */
 public class StringReplaceBlank04 {
-
-
     public String replaceSpace(StringBuffer str) {
         // write your code here
         int countBlank=0;
@@ -23,7 +22,6 @@ public class StringReplaceBlank04 {
             }
         }
         char[] chars = new char[countBlank*2+str.length()];
-
         //char[] a = string.toCharArray()
         //strLength是记录原字符串下标范围
         int strLength =str.length()-1;
@@ -38,11 +36,9 @@ public class StringReplaceBlank04 {
             }
             --strLength;
         }
-
         //将一个char数组转换成String 不能用chars.toString()
         return String.valueOf(chars);
     }
-
     public static void main(String[] args) {
         StringBuffer string = new StringBuffer("Mrs john hello");
         StringReplaceBlank04 stringReplaceBlank04 = new StringReplaceBlank04();
