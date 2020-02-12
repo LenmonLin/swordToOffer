0e4c2e89b9f2a14c5f7c2a940ce9bd2fa08d0b37
lemonLin <linfromearth@outlook.com>
2020-02-12 11:40:33 +0800

    LeetCode公共节点，建树

--- src/common/MakeTree.java
+++ src/common/MakeTree.java
@@ -30,7 +30,25 @@ public class MakeTree {
 
         return treeNodeCommon1;
     }
+    public  TreeNode makeTree2(){
+        TreeNode treeNodeCommon1 = new TreeNode(8);
+        TreeNode treeNodeCommon2 = new TreeNode(6);
+        TreeNode treeNodeCommon3 = new TreeNode(10);
+//        TreeNodeCommon treeNodeCommon4 = new TreeNodeCommon(5);
+        TreeNode treeNodeCommon5 = new TreeNode(7);
+        TreeNode treeNodeCommon6 = new TreeNode(9);
+        TreeNode treeNodeCommon7 = new TreeNode(11);
 
+        treeNodeCommon1.left = treeNodeCommon2;
+        treeNodeCommon1.right = treeNodeCommon3;
+        treeNodeCommon2.left = null;
+        treeNodeCommon2.right = treeNodeCommon5;
+
+        treeNodeCommon3.left = treeNodeCommon6;
+        treeNodeCommon3.right = treeNodeCommon7;
+
+        return treeNodeCommon1;
+    }
     public static void main(String[] args) {
 
     }
---
+++ src/common/TreeNode.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * @author LemonLin
+ * @Description :TreeNode
+ * @date 20.2.12-11:26
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
