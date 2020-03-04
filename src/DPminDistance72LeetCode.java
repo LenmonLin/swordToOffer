2967c5e69e28882b8829356ea93a5df27f004ec7
lemonLin <linfromearth@outlook.com>
2020-03-04 21:52:04 +0800

    给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。

---
+++ src/DPminDistance72LeetCode.java
@@ -0,0 +1,80 @@
+/**
+ * 给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * 示例 1:
+ * 输入: word1 = "horse", word2 = "ros"
+ * 输出: 3
+ * 解释:
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * 示例 2:
+ * 输入: word1 = "intention", word2 = "execution"
+ * 输出: 5
+ * 解释:
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ * @author LemonLin
+ * @Description :DPminDistance72LeetCode
+ * @date 20.2.29-16:31
+ * 思路：参考：https://leetcode-cn.com/problems/edit-distance/solution/
+ * bian-ji-ju-chi-mian-shi-ti-xiang-jie-by-labuladong/
+ * 1、解决两个字符串的动态规划问题，一般都是用两个指针 i,j 分别指向两个字符串的最
+ * 后，然后一步步往前走，缩小问题的规模。
+ * 2、设置dp table,一般题目所求什么就设置什么。(除了300题，比较特殊)
+ * 状态设置dp[i][j]为word1的长度为[0,i]和word2的长度为[0,j]的最小编辑距离。
+ * 那么可以得出一张dp table
+ *       h o r  s  e
+ *       0,1,i,...
+ * r  0
+ * o  1
+ * s  j
+ *    ...
+ * 3、用数学归纳法想问题，如果得到了dp[i-1][j-1]怎么得到dp[i][j]
+ * 状态转移方程就是i,j从前往后遍历：
+ * if w1[i-1] == w2[j-1]:
+ *      dp[i][j] = dp[i-1][j-1]  //因为什么都不用做，所以是都往前移动
+ * else:
+ *     选min(插，删，换)
+ *3、边界条件：
+ *   如果是i=0,j=0,那么不用任何操作，所以dp[0][0]=0;
+ */
+public class DPminDistance72LeetCode {
+    public int minDistance(String word1, String word2) {
+        int length1= word1.length();
+        int length2 = word2.length();
+        int [][] dp = new int[length1+1][length2+1];
+        //注意这里边界从1开始，
+        for (int i=1;i<=length1;i++){
+            dp[i][0]=i;
+        }
+        for (int j=1;j<=length2;j++){
+            dp[0][j]=j;
+        }
+        //同时这里不考虑i=0和j=0的情况，因为没有意义。
+        for (int i =1;i<=length1;i++){
+            for (int j =1;j<=length2;j++){
+                //注意这里比较的是i-1和j-1
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    dp[i][j] = Math.min(dp[i-1][j]+1,
+                            Math.min(dp[i][j-1]+1,dp[i-1][j-1]+1));
+                }
+            }
+        }
+        return dp[length1][length2];
+    }
+
+    public static void main(String[] args) {
+        String word1="horse";
+        String word2 ="ros";
+        System.out.println(new DPminDistance72LeetCode().minDistance(word1, word2));
+    }
+}
