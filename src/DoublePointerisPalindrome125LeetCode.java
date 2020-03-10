0f761ad7b9e2833cb282c7694c0798b90f99835e
lemonLin <linfromearth@outlook.com>
2020-03-10 20:20:40 +0800

    给定一个字符串， 判定它是否是回文（只统计字母、数字，其他字符请忽略）

--- src/DoublePointerisPalindrome125LeetCode.java
+++ src/DoublePointerisPalindrome125LeetCode.java
@@ -1,7 +1,7 @@
 /**
  * 给定一个字符串， 判定它是否是回文（只统计字母、数字，其他字符请忽略）
  * @author LemonLin
- * @Description :StringValidPalindrome
+ * @Description :DoublePointerisPalindrome125LeetCode
  * @date 2019/5/9-16:40
  * Given a string, determine if it is a palindrome, considering only alphanumeric characters
  * and ignoring cases.
@@ -13,10 +13,13 @@
  *Example 2:
  *      Input: "race a car"
  *      Output: false
- * 解题思路：采用二分法，一个指针从左边遍历，一个从右边遍历，跳过非字母和非数字，当遍历到中点依
- * 然相同那就是回文。时间复杂度O(n), 空间复杂度O(1)
+ * 解题思路：采用双指针方法，一个指针从左边遍历，一个从右边遍历，跳过非字母和非数
+ * 字，当遍历到中点依然相同那就是回文。时间复杂度O(n), 空间复杂度O(1)。主要中间
+ * 要处理一些细节问题：
+ * 1、比如对字母、数字的判断。不是字母和数字或者是空格的，都直接跳过。
+ * 2、要全部转换为小写字符来进行判断
  */
-public class StringValidPalindrome125LeetCode {
+public class DoublePointerisPalindrome125LeetCode {
     public boolean isPalindrome(String s) {
         if(s.isEmpty()){
             return true;
@@ -51,7 +54,7 @@ public class StringValidPalindrome125LeetCode {
     public static void main(String[] args) {
         String test = "A man, a plan, a canal: Panama";
         String test2 =" ";
-        boolean result = new StringValidPalindrome125LeetCode().isPalindrome(test2);
+        boolean result = new DoublePointerisPalindrome125LeetCode().isPalindrome(test2);
         System.out.println(result);
     }
 }
