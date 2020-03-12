f62c14f2a8f947816cf7f352e0249261362491a7
lemonLin <linfromearth@outlook.com>
2020-03-12 23:37:37 +0800

    给定一个二叉树，判断它是否是高度平衡的二叉树。

--- src/TreeisBalanced110LeetCode.java
+++ src/TreeisBalanced110LeetCode.java
@@ -1,3 +1,6 @@
+import common.MakeTree;
+import common.TreeNode;
+
 /**
 *  * 给定一个二叉树，判断它是否是高度平衡的二叉树。
 *  本题中，一棵高度平衡二叉树定义为：
@@ -23,35 +26,54 @@
  * 思路：总体思路是求某个节点的树高，这里利用求左子树树高和右子树树高的较大值加1，
  * 为某个节点的树高，求完树高之后，遍历比较每个节点的左子树和右子树的树高之差的
  * 绝对值是不是小于等于1，来判断是否平衡。
- * 改进思路：这里有个问题，就是要求所有结点的树高，其实只要比较有某个结点达到不平衡
- * 的条件就可以退出递归，不需要求出所有结点的树高，有点类似剪枝的意思
  * @author LemonLin
  * @Description :TreeisBalanced110LeetCode
  * @date 2020/1/9-20:00
  */
 public class TreeisBalanced110LeetCode {
     // Definition for a binary tree node.
-    public class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
-     }
+//    public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x) { val = x; }
+//     }
      public boolean isBalanced(TreeNode root) {
         if (root == null){
             return true;
         }
         int leftHeight = treeHeight(root.left);
         int  rightHeight = treeHeight(root.right);
-        //这里要注意判断是否是平衡二叉树需要节点本身满足平衡二叉树同时左子树满足平衡
-         //右子树满足平衡，要三者与的关系，不要想漏了
-        return Math.abs(leftHeight-rightHeight)<2&&isBalanced(root.left)&&
-                isBalanced(root.right);
+        if (Math.abs(leftHeight-rightHeight)>=2){
+            return false;
+        }
+        /**这里不能写返回true，否则如果是
+         *    1
+         *  2   3
+         *4       5
+         *6         7
+         * 的情况下就会返回true，实际是false,因为这种递归函数，只要有一个返回值return
+         * 就会终止整个递归函数。而且如果某个节点是满足平衡，但是也不能代表这个树满足
+         * 平衡，但是如果某个节点不满足平衡，那么整科树一定不平衡。然后把递归满足出口
+         * 设置在root == null，叶子节点上，只有前面的节点都没有问题，那么到了叶子节点
+         * 自然应该返回true。
+         * 这题和LeetCode100的区别，LeetCode100都是到了叶子节点才能判断对错，而本题
+         * 在中途就能判断错。但是只能在叶子节点判断错误。
+        */
+//        if (Math.abs(leftHeight-rightHeight)<2){
+//            return true;
+//        }
+        return isBalanced(root.left)&&isBalanced(root.right);
      }
     public  int  treeHeight(TreeNode treeNode){
         if (treeNode == null)return 0;
         int  leftHeight = treeHeight(treeNode.left);
         int rightHeight = treeHeight(treeNode.right);
         return Math.max(leftHeight,rightHeight)+1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new MakeTree().makeTree2();
+        System.out.println(new TreeisBalanced110LeetCode().isBalanced(treeNode));
     }
 }
