ab34c6b5a95903b61da48444460900ca65a5e539
lemonLin <linfromearth@outlook.com>
2020-05-07 09:13:20 +0800

    不同的二叉搜索树

--- src/DPnumTrees96LeetCode.java
+++ src/DPnumTrees96LeetCode.java
@@ -15,12 +15,11 @@
  * @date 20.1.31-19:07
  * 思路:神奇的卡特兰数：
  * 假设n个节点存在二叉排序树的个数是G(n)，令f(i)为以i为根的二叉搜索树的个数，则
- * G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)G(n)=f(1)+f(2)+f(3)+f(4)+...+f(n)
+ * G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)
  *当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，则
- * f(i) = G(i-1)*G(n-i)f(i)=G(i−1)∗G(n−i)
+ * f(i) = G(i-1)*G(n-i)
  * 综合两个公式可以得到 卡特兰数 公式
- * G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)G(n)=G(0)∗G(n−1)+G(1)∗
- * (n−2)+...+G(n−1)∗G(0)
+ * G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
  * 参考：https://leetcode-cn.com/problems/unique-binary-search-trees/
  * solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
  */
@@ -30,7 +29,7 @@ public class DPnumTrees96LeetCode {
         //注意初始化0个节点也是1。
         dp[0]=1;
         dp[1]=1;
-        //下标根据卡特兰公式用几个特殊数字带入即可求得
+        //下标根据卡特兰公式用几个特殊数字带入即可求得，这里的双循环下标有点难想明白。
         for (int i=2;i<=n;i++){
             for (int j=1;j<=i;j++){
                 dp[i]+=dp[j-1]*dp[i-j];
