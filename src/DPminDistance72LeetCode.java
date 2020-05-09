41af5c512d4a035fc54ce6430aa7c389d374937b
lemonLin <linfromearth@outlook.com>
2020-05-09 11:27:07 +0800

    给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。

--- src/DPminDistance72LeetCode.java
+++ src/DPminDistance72LeetCode.java
@@ -44,6 +44,32 @@
  *     选min(插，删，换)
  *3、边界条件：
  *   如果是i=0,j=0,那么不用任何操作，所以dp[0][0]=0;
+ *
+ *   关于下标的理解：参考https://leetcode-cn.com/problems/edit-distance/solution/zi-di-xiang-shang-he-zi-ding-xiang-xia-by-powcai-3/
+ *   讲一下我自己对状态转移方程的理解,麻烦大家看看我说得对不对有没有道理:
+ * (一)、当word1[i]==word2[j]时,由于遍历到了i和j,说明word1的0~i-1和word2的0~j-1的匹配结果已经生成,
+ * 由于当前两个字符相同,因此无需做任何操作,dp[i][j]=dp[i-1][j-1]
+ * (二)、当word1[i]!=word2[j]时,可以进行的操作有3个:
+ *       ① 替换操作:可能word1的0~i-1位置与word2的0~j-1位置的字符都相同,
+ *            只是当前位置的字符不匹配,进行替换操作后两者变得相同,
+ *            所以此时dp[i][j]=dp[i-1][j-1]+1(这个加1代表执行替换操作)
+ *       ②删除操作:若此时word1的0~i-1位置与word2的0~j位置已经匹配了,
+ *          此时多出了word1的i位置字符,应把它删除掉,才能使此时word1的0~i(这个i是执行了删除操作后新的i)
+ *          和word2的0~j位置匹配,因此此时dp[i][j]=dp[i-1][j]+1(这个加1代表执行删除操作)
+ *       ③插入操作:若此时word1的0~i位置只是和word2的0~j-1位置匹配,
+ *           此时只需要在原来的i位置后面插入一个和word2的j位置相同的字符使得
+ *           此时的word1的0~i(这个i是执行了插入操作后新的i)和word2的0~j匹配得上,
+ *           所以此时dp[i][j]=dp[i][j-1]+1(这个加1代表执行插入操作)
+ *       ④由于题目所要求的是要最少的操作数:所以当word1[i] != word2[j] 时,
+ *           需要在这三个操作中选取一个最小的值赋格当前的dp[i][j]
+ * (三)总结:状态方程为:
+ * if(word1[i] == word2[j]):
+ *       dp[i][j] = dp[i-1][j-1]
+ * else:
+ *        min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1
+ * PS:大佬的代码中word1.charAt(i-1)==word2.charAt(j-1)的原因是:
+ *      初始化DP Table时dp[i][0]和dp[0][j]已经填写完成,所以接下来填表需要从1开始,
+ *      但是字符的比较需要从0开始,因此才这样子写
  */
 public class DPminDistance72LeetCode {
     public int minDistance(String word1, String word2) {
