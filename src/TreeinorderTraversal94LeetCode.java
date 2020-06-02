c4012d997fe1596978d3154a0ab905d39ef483ca
lemonLin <linfromearth@outlook.com>
2020-06-02 22:53:12 +0800

    给定一个二叉树，返回它的中序 遍历。

--- src/TreeinorderTraversal94LeetCode.java
+++ src/TreeinorderTraversal94LeetCode.java
@@ -1,5 +1,3 @@
-import common.TreeNode;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,17 +19,19 @@ import java.util.List;
  * 思路：主要考察非递归迭代写法
      * 参考：https://segmentfault.com/a/1190000016674584
  * 主要解决先存根节点，然后循环不断存左节点，以及左结点的左结点的问题
+ * 参考：https://www.icourse163.org/learn/ZJU-93001?tid=1450069451#/learn/content?type=detail&id=1214143618&cid=1217772376&replay=true
  */
 public class TreeinorderTraversal94LeetCode {
     /**
      * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode(int x) { val = x; }
-     * }
-     */
+     * */
+     static class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
         LinkedList<TreeNode> stack = new LinkedList();
@@ -44,10 +44,37 @@ public class TreeinorderTraversal94LeetCode {
                 stack.addLast(cur);
                 cur = cur.left;
             }
-            cur = stack.pollLast();
-            result.add(cur.val);
-            cur = cur.right;
+            if (!stack.isEmpty()){
+                cur = stack.pollLast();
+                result.add(cur.val);
+                cur = cur.right;
+            }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        /**
+         *                   8
+         *              6       10
+         *           null   7    9    11
+         *           中序输出：null 6,7,8,9,10,11
+         */
+        TreeNode treeNodeCommon1 = new TreeNode(8);
+        TreeNode treeNodeCommon2 = new TreeNode(6);
+        TreeNode treeNodeCommon3 = new TreeNode(10);
+        TreeNode treeNodeCommon4 = new TreeNode(5);
+        TreeNode treeNodeCommon5 = new TreeNode(7);
+        TreeNode treeNodeCommon6 = new TreeNode(9);
+        TreeNode treeNodeCommon7 = new TreeNode(11);
+
+        treeNodeCommon1.left = treeNodeCommon2;
+        treeNodeCommon1.right = treeNodeCommon3;
+        treeNodeCommon2.left = null;
+        treeNodeCommon2.right = treeNodeCommon5;
+
+        treeNodeCommon3.left = treeNodeCommon6;
+        treeNodeCommon3.right = treeNodeCommon7;
+        new TreeinorderTraversal94LeetCode().inorderTraversal(treeNodeCommon1);
     }
 }
